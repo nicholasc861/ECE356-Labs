cd6513fe2f967040e5a9b213b999d84ad1066aa0
Nicholas Chung-Hun <nicholas.chunghun@gmail.com>
2022-11-28 18:20:11 -0500

    Initial query for extracting features for data classification

---
+++ Lab 4/extraction.sql
@@ -0,0 +1,25 @@
+-- Task A
+SELECT * FROM
+(
+    SELECT pe.playerID, SUM(B.G) as gamesBatted, AVG(B.RBI) as rbi, COUNT(*) as years
+    FROM People pe
+    LEFT JOIN Batting B on pe.playerID = B.playerID
+    WHERE pe.playerID in (SELECT DISTINCT h.playerID
+                        FROM HallOfFame h)
+    GROUP BY pe.playerID) as A
+JOIN (
+    SELECT pe.playerID, SUM(P.G) as gamesPitched, COUNT(*) as years
+    FROM People pe
+    LEFT JOIN Pitching P on pe.playerID = P.playerID
+    WHERE pe.playerID in (SELECT DISTINCT h.playerID
+                        FROM HallOfFame h)
+    GROUP BY pe.playerID) as B
+ON A.playerID = B.playerID
+JOIN (
+    SELECT pe.playerID, SUM(F.G) as gamesFielded, COUNT(*) as years
+    FROM People pe
+    LEFT JOIN Fielding F on pe.playerID = F.playerID
+    WHERE pe.playerID in (SELECT DISTINCT h.playerID
+                        FROM HallOfFame h)
+    GROUP BY pe.playerID) as C
+ON A.playerID = C.playerID;
