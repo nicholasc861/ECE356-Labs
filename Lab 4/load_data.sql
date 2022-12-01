d2b7f7bab4b03fa94eb281e31e21cfc17e1bd09e
Ernest Ho <ernest.ho.001@gmail.com>
2022-12-01 13:55:03 -0500

    finished tables on load_data

--- Lab 4/load_data.sql
+++ Lab 4/load_data.sql
@@ -53,6 +53,86 @@ CREATE TABLE Batting
     GIDP     int  null
 );
 
+CREATE TABLE People (
+    playerID     text    null,
+    birthYear    int     null,
+    birthMonth   int     null,
+    birthDay     int     null,
+    birthCountry text    null,
+    birthState   text    null,
+    birthCity    text    null,
+    deathYear    int     null,
+    deathMonth   int     null,
+    deathDay     int     null,
+    deathCountry text    null,
+    deathState   text    null,
+    deathCity    text    null,
+    nameFirst    text    null,
+    nameLast     text    null,
+    nameGiven    text    null,
+    weight       int     null,
+    height       int     null,
+    bats         int     null,
+    throws       int     null,
+    debut        text    null,
+    finalGame    text    null,
+    retroID      text    null,
+    bbrefID      text    null
+);
+
+CREATE TABLE Pitching (
+    playerID text null,
+    yearID int null,
+    stint int null,
+    teamID text null,
+    lgID text null,
+    W int null,
+    L int null,
+    G int null,
+    GS int null,
+    CG int null,
+    SHO int null,
+    SV int null,
+    IPouts int null,
+    H int null,
+    ER int null,
+    HR int null,
+    BB int null,
+    SO int null,
+    BAOpp int null,
+    ERA float null,
+    IBB int null,
+    WP int null,
+    HBP int null,
+    BK int null,
+    BFP int null,
+    GF int null,
+    R int null,
+    SH int null,
+    SF int null,
+    GIDP int null
+);
+
+CREATE TABLE Fielding (
+    playerID text null,
+    yearID int null,
+    stint int null,
+    teamID text null,
+    lgID text null,
+    POS text null,
+    G int null,
+    GS int null,
+    InnOuts int null,
+    PO int null,
+    A int null,
+    E int null,
+    DP int null,
+    PB int null,
+    WP int null,
+    SB int null,
+    CS int null,
+    ZR int null
+);
 -- Import People.csv
 LOAD DATA LOCAL INFILE "./baseball_data/People.csv"
 INTO TABLE People
